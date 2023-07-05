4098c3bb9abc55cf401f8a529bfc8d8155040fb7
かっしー <106821691+kasshii28@users.noreply.github.com>
2023-07-05 20:50:37 +0900

    ♻️ Linksの修正

--- components/Links.jsx
+++ components/Links.jsx
@@ -1,21 +1,47 @@
 import styles from './Links.module.css'
 
+const LINKS = [
+    { href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+      title: "Docs →",
+      description: "Find in-depth information about Next.js features and&nbsp;API."
+    },
+    {
+      href: "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+      title: "Learn →",
+      description: "Learn about Next.js in an interactive course with&nbsp;quizzes!"
+    },
+    {
+      href: "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+      title: "Templates →",
+      description: "Discover and deploy boilerplate example Next.js&nbsp;projects."
+    },
+    {
+      href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+      title: "Deploy →",
+      description: "Instantly deploy your Next.js site to a shareable URLwith&nbsp;Vercel."
+    },
+];
 export const Links = (() =>{
     return (
     <div className={styles.grid}>
-            <a
-                href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                className={styles.card}
-                target="_blank"
-                rel="noopener noreferrer"
-            >
+        {LINKS.map(link =>{
+            return (
+                <a
+                    key={link.href}
+                    href={link.href}
+                    className={styles.card}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                 <h2>
-                Docs <span>-&gt;</span>
+                {link.title}
                 </h2>
                 <p>
-                Find in-depth information about Next.js features and&nbsp;API.
-                </p>
-            </a>
+                {link.description}
+                </p>   
+                </a>
+            )
+        })}
 
             <a
                 href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -23,12 +49,6 @@ export const Links = (() =>{
                 target="_blank"
                 rel="noopener noreferrer"
             >
-                <h2>
-                Learn <span>-&gt;</span>
-                </h2>
-                <p>
-                Learn about Next.js in an interactive course with&nbsp;quizzes!
-                </p>
             </a>
 
             <a
@@ -37,12 +57,7 @@ export const Links = (() =>{
                 target="_blank"
                 rel="noopener noreferrer"
             >
-                <h2>
-                Templates <span>-&gt;</span>
-                </h2>
-                <p>
-                Discover and deploy boilerplate example Next.js&nbsp;projects.
-                </p>
+                
             </a>
 
             <a
@@ -51,13 +66,7 @@ export const Links = (() =>{
                 target="_blank"
                 rel="noopener noreferrer"
             >
-                <h2>
-                Deploy <span>-&gt;</span>
-                </h2>
-                <p>
-                Instantly deploy your Next.js site to a shareable URL
-                with&nbsp;Vercel.
-            </p>
+                
         </a>
     </div>
     )
