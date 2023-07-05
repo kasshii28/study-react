ff75721bf6fbc354aae1bfecbfc1cbfd9cc1a0f6
かっしー <106821691+kasshii28@users.noreply.github.com>
2023-07-05 21:50:10 +0900

    ♻️ ルーティングの修正

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
-import { Footer } from '../../components/Footer';
-import { Main } from '../../components/Main';
-import { Header } from '../../components/Header';
+import { Footer } from '../components/Footer';
+import { Main } from '../components/Main';
+import { Header } from '../components/Header';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header/>
-      <Main page="index"/>
+      <Main page="about"/>
 
       <Footer/>
     </>
