a27aa1c7c5c51fa50993711bad83cf0f12e4e1b6
かっしー <106821691+kasshii28@users.noreply.github.com>
2023-07-08 16:33:16 +0900

    同じく

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,7 +8,7 @@ import { useCallback , useState , useEffect} from 'react';
 export default function Home() {
   const [count, setCount] = useState(1)
   
-  const handleClick = useCallback((e)=>{
+  const handleClick = useCallback(()=>{
     if(count<10){
       setCount((count)=>count+1);
     }
